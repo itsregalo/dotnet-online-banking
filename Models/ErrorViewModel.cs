9aeaf42cf231a0bc359fafee7ed1b28855817c90
itsregalo <itsregalo047@gmail.com>
2023-08-05 16:17:59 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace online_banking.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
